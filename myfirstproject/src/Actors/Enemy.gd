a4cc61e2aade8770624976d6fbc0b75d8d5a0da3
Eggsy07 <88040864+Eggsy07@users.noreply.github.com>
2023-05-19 22:36:11 +0200

    Add files via upload

---
+++ myfirstproject/src/Actors/Enemy.gd
@@ -0,0 +1,26 @@
+extends "res://src/Actors/Actor.gd"
+
+export var score: = 100
+
+func _ready():
+	set_physics_process(false)
+	_velocity.x = -speed.x
+
+
+func _on_StompDetector_body_entered(body: PhysicsBody2D) -> void:
+	if body.global_position.y > get_node("StompDetector").global_position.y:
+		return
+	get_node("CollisionShape2D").set_deferred("disabled", true)
+	PlayerData.score += score
+	queue_free()
+
+
+func _physics_process(delta: float) -> void:
+	_velocity.y += gravity * delta
+	if is_on_wall():
+		_velocity.x *= -1.0
+	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
+
+func die() -> void:
+	PlayerData.score += score
+	queue_free()
