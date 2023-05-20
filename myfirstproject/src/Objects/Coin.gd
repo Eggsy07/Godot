fcdeb28c32847a184bd6a049100bab6b3235058f
Eggsy07 <88040864+Eggsy07@users.noreply.github.com>
2023-05-20 13:45:09 +0200

    Update Coin.gd

--- myfirstproject/src/Objects/Coin.gd
+++ myfirstproject/src/Objects/Coin.gd
@@ -2,8 +2,10 @@ extends Area2D
 
 onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
 export var score: = 100
-
+var cont: int = 0
 func _on_body_entered(body: PhysicsBody2D) -> void:
-	PlayerData.score += score
+	cont += 1
+	if cont == 1:
+		PlayerData.score += score
 	$AudioStreamPlayer.play()
 	anim_player.play("fade_out")
